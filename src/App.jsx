ba0961a0fe4911da6349f11ff37099cf8e10cd56
Numbcranium <fakuadesamuel28@gmail.com>
2025-09-01 12:03:09 +0100

    Still merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<ServicePage />} />
       </Routes>
-      <Footer/>
+      <Footer/>w3
     </Router>
   );
 }
