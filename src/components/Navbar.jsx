8ebeba1ea98eba7a4c111e59a787e7647ace4a20
Monarch-Codes00 <chinedumroster@gmail.com>
2025-09-01 21:44:59 +0930

    active class on nav and route linking for contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 
 function Navbar() {
+  const location = useLocation();
+  const isLightBg = location.pathname === "/" || location.pathname === "/about";
+
   return (
      <>
-      <nav className="navbar" style={{position: "relative"}}>
+      <nav className={`navbar ${isLightBg ? "light-bg" : ""}`} style={{position: "relative"}}>
       <div className="navbar-container" style={{ display: 'flex', justifyContent: '', alignItems: 'center',padding:"10px 20px"}}>
         <h1 className="navbar-title" ></h1>
-        <ul className="navbar-links" style={{ display: 'flex',fontSize:"18px", justifyContent: 'space-between', alignItems: 'center', listStyle:"none", textDecoration:"none", marginTop:"10px" }}> 
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services" style={{marginLeft:"60px", marginRight:"0px"}}>Services</Link></li>
+        <ul className="navbar-links" style={{ display: 'flex',fontSize:"18px", justifyContent: 'space-between', alignItems: 'center', listStyle:"none", textDecoration:"none", marginTop:"10px" }}>
+          <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/services" activeClassName="active" style={{marginLeft:"60px", marginRight:"0px"}}>Services</NavLink></li>
           <li><a href="#">Blog</a></li>
-          <li><a href="/contact">Contact</a></li>         
+          <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
 
         </ul>
         <ul style={{display:"flex", flexDirection:"row",gap:"30px", listStyle:"none"}}>
@@ -22,7 +25,7 @@ function Navbar() {
           <li className="cart-hold"></li>
          <li className="search-hold"></li>
 
-         
+
 
         </ul>
 
@@ -30,7 +33,7 @@ function Navbar() {
 
       </div>
     </nav>
-   
+
       </>
   )
 }
