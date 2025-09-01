bf4534a6b065d54c183c5efcb1de517d90964ebb
Numbcranium <fakuadesamuel28@gmail.com>
2025-09-01 12:18:22 +0100

    mistake

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<ServicePage />} />
       </Routes>
-      <Footer/>w3
+      <Footer/>
     </Router>
   );
 }
