340b90ce07a55b31fb660591c2fe1bcc95bda58d
Monarch-Codes00 <chinedumroster@gmail.com>
2025-09-01 14:02:05 +0930

    `Added About page content and styles: updated About.jsx and added About.css and modified Navbar.css`

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,10 @@ function About() {
   return (
    <>
    <div className="AboutUs-container">
-  
-  
-   
+     <div className="about-top-section">
+      <p className="top-text-about">About us</p>
+      <p className="lower-text-about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, adipisci quas dignissimos voluptatib</p>
+     </div>
    </div>
    </>
   );
